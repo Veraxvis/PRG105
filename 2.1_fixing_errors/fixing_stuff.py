8bd1c7213ad2e255a0d72fcf00b7fd7ef385d73f
Daniel O'Grady <dogrady@students.mchenry.edu>
2018-09-01 16:47:36 -0500

    Change 1.0.1

---
+++ 2.1_fixing_errors/fixing_stuff.py
@@ -0,0 +1,16 @@
+print("\nPray (Put em in the dirt) - By Hollywood Undead\n")
+
+print("Let me tell you a story, it's about a little kid")
+print("Who never really understood much of anything he did")
+print("He had a chest full of heart and a body full of scars")
+print("In a broke down palace on a broken boulevard\n")
+
+print("And he was faithless, but he lived through all the hurt")
+print("'Cause every time he did he knew he got what he deserved")
+print("Had to hold onto the fire even if he had to burn")
+print("Any pain became the only way that he could ever learn\n")
+
+print("So thankless, but he knew that he could take this")
+print("Everything then came apart so one day he could make this")
+print("It's a song for the nameless, that's ageless")
+print("Hold onto my hand, I'll show you scripture in these pages")
---
+++ chapter_2_practice/TODO.py
@@ -0,0 +1,36 @@
+# TO DO 2.3.1 {DISPLAY OUTPUT WITH STRING FUNCTION}
+print("Hello, my name is Daniel O'Grady")
+print("My major is computer science")
+
+# TO DO 2.3.2 {USING QUOTES}
+print("The dog says woof!")
+
+# TO DO 2.4.1 {WORKING WITH AND PRINTING VARIABLES
+age = 18
+print(age)
+print("Age", age)
+age = 42
+print(age)
+print("Age", age)
+
+# TO DO 2.6.1 {KEYBOARD INPUT}
+name = input("What is your name?")
+print("Hello, " + name + "!")
+
+# TO DO 2.6-2.7 {NUMERIC INPUT, PREFORMING CALCULATIONS}
+data = int(input("Enter your age: "), 10)
+print("This year you are ", data)
+data2 = data + 1
+print("Next year you will be " + str(data2))
+
+# TO DO 2.7 {PERFORMING CALCULATIONS}
+doMath = 7 / 2
+print("7 / 2 = " + str(doMath))
+doMathToo = 7 % 2
+print("7 % 2 = " + str(doMathToo))
+
+# TO DO 2.7 {DATA CONVERSION}
+print("5 + 8 = " + str(5 + 8))
+print("4 / 2 = " + str(4 / 2))
+print("11.25 / .125 = " + str(11.25 / .125))
+print("3.5 / 2 = " + str(3.5 / 2))
